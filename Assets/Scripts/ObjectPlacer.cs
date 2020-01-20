fd2ac19c5357932700e67193e097ba7e3d8bbb9a
Andrej Perfilov [home] <33530333+p3rfilov@users.noreply.github.com>
2020-01-20 22:29:25 +0000

    add missing neighbours in Triangle grid model

--- Assets/Scripts/ObjectPlacer.cs
+++ Assets/Scripts/ObjectPlacer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [DisallowMultipleComponent]
 public class ObjectPlacer : MonoBehaviour
@@ -34,6 +35,114 @@ public class ObjectPlacer : MonoBehaviour
     private void Start ()
     {
         PlaceObjects();
+    }
+
+    public void PlaceObjects ()
+    {
+        Init();
+        if (objects != null && objects.Length > 0)
+        {
+            Vector3 pos;
+            Vector3 rot;
+            Vector3 scale;
+
+            Vector3 parentPosition = transform.position;
+            float half_spacing = spacing / 2;
+            float tri_height = Mathf.Sqrt(spacing * spacing - half_spacing * half_spacing);
+
+            for (int y = 0; y < yCount; y++)
+            {
+                for (int x = 0; x < xCount; x++)
+                {
+                    pos = parentPosition;
+                    if (gridCellType == GridCellType.Triangle)
+                    {
+                        float offset = 0;
+                        if (y % 2 != 0) // shift every odd row halfway forward
+                        {
+                            offset = half_spacing;
+                        }
+                        pos += new Vector3(x * spacing + offset, 0f, y * tri_height);
+                    }
+                    else
+                    {
+                        pos += new Vector3(x * spacing, 0f, y * spacing);
+                    }
+
+                    pos = GetRayHitPositionOnSurface(pos);
+                    pos += GetRandomValue(randomPositionOffset);
+                    rot = initialRotation + GetRandomValue(randomRotationOffset);
+                    scale = initialScale + GetRandomValue(randomScaleOffset);
+
+                    SwarmObject _obj = objects[Random.Range(0, objects.Length)];
+                    if (_obj != null)
+                    {
+                        GameObject obj = Instantiate(_obj.gameObject, pos, Quaternion.Euler(rot), transform);
+                        obj.transform.localScale = scale;
+
+                        var swarwObj = obj.GetComponent<SwarmObject>();
+                        swarwObj.SetInitialPosition(pos);
+                        swarwObj.SetIndex(new Vector2Int(x, y));
+                        swarmObjectArray[x, y] = swarwObj;
+                    }
+                }
+            }
+        }
+    }
+
+    public List<SwarmObject>[] GetNeighbours (SwarmObject obj, int count)
+    {
+        var array = new List<SwarmObject>[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            array[i] = new List<SwarmObject>();
+            if (i == 0)
+            {
+                array[i].Add(obj);
+            }
+            else
+            {
+                foreach (SwarmObject item in array[i - 1])
+                {
+                    Vector2Int[] nIndexes;
+                    var i1 = new Vector2Int(item.Index.x + 1, item.Index.y);
+                    var i2 = new Vector2Int(item.Index.x - 1, item.Index.y);
+                    var i3 = new Vector2Int(item.Index.x, item.Index.y + 1);
+                    var i4 = new Vector2Int(item.Index.x, item.Index.y - 1);
+
+                    if (gridCellType == GridCellType.Square)
+                    {
+                        nIndexes = new Vector2Int[] { i1, i2, i3, i4 };
+                    }
+                    else // due to the grid being shifted halfway forward in y, we get 2 additional (odd/even row-dependant) neighbours at y + 1 and y + 1
+                    {
+                        var i5 = new Vector2Int(item.Index.x + (item.Index.y % 2 != 0 ? 1 : -1), item.Index.y + 1);
+                        var i6 = new Vector2Int(item.Index.x + (item.Index.y % 2 != 0 ? 1 : -1), item.Index.y - 1);
+                        nIndexes = new Vector2Int[] { i1, i2, i3, i4, i5, i6 };
+                    }
+
+                    foreach (var index in nIndexes)
+                    {
+                        if (_IsIndexInRange(index))
+                        {
+                            SwarmObject neighbour = swarmObjectArray[index.x, index.y];
+                            if (!_IsObjectCollected(neighbour, array, i))
+                            {
+                                array[i].Add(neighbour);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+    private Vector3 GetRandomValue (Vector3 range)
+    {
+        var result = new Vector3(Random.Range(range.x, -range.x), Random.Range(range.y, -range.y), Random.Range(range.z, -range.z));
+        return result;
     }
 
     private void Init ()
@@ -77,62 +186,28 @@ public class ObjectPlacer : MonoBehaviour
         return Vector3.down;
     }
 
-    public void PlaceObjects ()
+    private bool _IsIndexInRange (Vector2Int index)
     {
-        Init();
-        if (objects != null && objects.Length > 0)
+        if (Enumerable.Range(0, xCount).ToList().Contains(index.x) && Enumerable.Range(0, yCount).ToList().Contains(index.y))
         {
-            Vector3 pos;
-            Vector3 rot;
-            Vector3 scale;
-
-            Vector3 parentPosition = transform.position;
-            float half_spacing = spacing / 2;
-            float tri_height = Mathf.Sqrt(spacing * spacing - half_spacing * half_spacing);
-
-            for (int y = 0; y < yCount; y++)
-            {
-                for (int x = 0; x < xCount; x++)
-                {
-                    pos = parentPosition;
-                    if (gridCellType == GridCellType.Triangle)
-                    {
-                        float offset = 0;
-                        if (y % 2 != 0)
-                        {
-                            offset = half_spacing;
-                        }
-                        pos += new Vector3(x * spacing + offset, 0f, y * tri_height);
-                    }
-                    else
-                    {
-                        pos += new Vector3(x * spacing, 0f, y * spacing);
-                    }
-
-                    pos = GetRayHitPositionOnSurface(pos);
-                    pos += GetRandomValue(randomPositionOffset);
-                    rot = initialRotation + GetRandomValue(randomRotationOffset);
-                    scale = initialScale + GetRandomValue(randomScaleOffset);
-
-                    SwarmObject _obj = objects[Random.Range(0, objects.Length)];
-                    if (_obj != null)
-                    {
-                        GameObject obj = Instantiate(_obj.gameObject, pos, Quaternion.Euler(rot), transform);
-                        obj.transform.localScale = scale;
-
-                        var swarwObj = obj.GetComponent<SwarmObject>();
-                        swarwObj.SetInitialPosition(pos);
-                        swarwObj.SetIndex(new Vector2Int(x, y));
-                        swarmObjectArray[x, y] = swarwObj;
-                    }
-                }
-            }
+            return true;
         }
+        return false;
     }
 
-    Vector3 GetRandomValue (Vector3 range)
+    private bool _IsObjectCollected (SwarmObject obj, List<SwarmObject>[] array, int index, int searchDepth = 1)
     {
-        var result = new Vector3(Random.Range(range.x, -range.x), Random.Range(range.y, -range.y), Random.Range(range.z, -range.z));
-        return result;
+        for (int i = index + 1, j = 0; i-- > 0; j++)
+        {
+            if (array[i].Contains(obj))
+            {
+                return true;
+            }
+            if (j > searchDepth)
+            {
+                break;
+            }
+        }
+        return false;
     }
 }
--- Assets/Scripts/SwarmController.cs
+++ Assets/Scripts/SwarmController.cs
@@ -31,7 +31,7 @@ public class SwarmController : MonoBehaviour
                 SwarmObject obj = GetObjectUnderMouse();
                 if (obj != null)
                 {
-                    List<SwarmObject>[] neighbours = GetNeighbours(obj);
+                    List<SwarmObject>[] neighbours = objectPlacer.GetNeighbours(obj, size);
                     foreach (var list_item in neighbours)
                     {
                         foreach (SwarmObject item in list_item)
@@ -57,70 +57,5 @@ public class SwarmController : MonoBehaviour
             }
         }
         return null;
-    }
-
-    private List<SwarmObject>[] GetNeighbours (SwarmObject obj)
-    {
-        var array = new List<SwarmObject>[size];
-        int grid_x = objectPlacer.xCount;
-
-        if (behaviour == OnClickBehaviour.Ripple)
-        {
-            for (int i = 0; i < size; i++)
-            {
-                array[i] = new List<SwarmObject>();
-                if (i == 0)
-                {
-                    array[i].Add(obj);
-                }
-                else
-                {
-                    foreach (SwarmObject item in array[i-1])
-                    {
-                        var i1 = new Vector2Int(item.Index.x + 1, item.Index.y);
-                        var i2 = new Vector2Int(item.Index.x - 1, item.Index.y);
-                        var i3 = new Vector2Int(item.Index.x, item.Index.y + 1);
-                        var i4 = new Vector2Int(item.Index.x, item.Index.y - 1);
-                        foreach (var index in new Vector2Int[] { i1, i2, i3, i4 })
-                        {
-                            if (_IsIndexInRange(index))
-                            {
-                                SwarmObject neighbour = objectPlacer.swarmObjectArray[index.x, index.y];
-                                if (!_IsObjectCollected(neighbour, array, i))
-                                {
-                                    array[i].Add(neighbour);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return array;
-    }
-
-    private bool _IsIndexInRange (Vector2Int index)
-    {
-        if (Enumerable.Range(0, objectPlacer.xCount).ToList().Contains(index.x) && Enumerable.Range(0, objectPlacer.yCount).ToList().Contains(index.y))
-        {
-            return true;
-        }
-        return false;
-    }
-
-    private bool _IsObjectCollected (SwarmObject obj, List<SwarmObject>[] array, int index, int searchDepth=1)
-    {
-        for (int i = index + 1, j = 0; i-- > 0; j++)
-        {
-            if (array[i].Contains(obj))
-            {
-                return true;
-            }
-            if (j > searchDepth)
-            {
-                break;
-            }
-        }
-        return false;
     }
 }
