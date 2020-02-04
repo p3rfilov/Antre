4da931f1fc611a822794b5623381fabf45d805a8
Andrej Perfilov [home] <33530333+p3rfilov@users.noreply.github.com>
2020-02-04 22:52:31 +0000

    lighting/performance test

---
+++ Assets/Scripts/LightSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SpawnTempLight (Vector3 position, Color color, float intensity, float lifetime)
+    {
+        GameObject lightGameObject = new GameObject("Temp Light");
+        Light lightComp = lightGameObject.AddComponent<Light>();
+        lightComp.color = color;
+        lightGameObject.transform.position = position;
+    }
+}
